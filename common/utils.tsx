0083fa0469c72b2e97eed20eddb7ac2361f6ab54
Farhad <rubelbd82@gmail.com>
2020-04-02 03:16:15 +0300

    common utils

---
+++ common/utils.tsx
@@ -0,0 +1,14 @@
+
+export const Colors = {
+    primary:    '#add536',
+    secondary:  '#488aff',
+    danger:     '#f53d3d',
+    light:      '#f4f4f4',
+    dark:       '#222',
+    facebook:   '#3b5998',
+    twitter:    '#00aced',
+    whatsapp:   '#075e54',
+    white:      '#ffffff',
+    gray:       '#C0C0C0'
+};
+
